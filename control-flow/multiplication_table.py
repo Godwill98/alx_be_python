6efd05df04f8386712a8d3e1ff48985b4c62d4ce
Godwill Kiplagat <godwillkiprop@gmail.com>
2024-09-21 10:13:46 +0300

    for loop added comments

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,5 +2,9 @@
 
 number = int(input("Enter a number to see its multiplication table:"))
 
+#! USE A FOR LOOP TO ITERATE THROUGH THE NUMBERS 1 TO 10.
 for i in range(1, 11):
+
+    #! Print each line of the multiplication table in the format: “X * Y = Z”
+    
     print(f"{number} x {i} = {number * i}")
