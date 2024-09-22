da9a8b742bc6c0da2a8b795f5f8bbbd70db4074f
Godwill Kiplagat <godwillkiprop@gmail.com>
2024-09-22 13:04:31 +0300

    modified the finance_calculator variables

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,16 +1,16 @@
 
 #PROMPT THE USER TO ENTER THEIR INCOME
 
-income = float(input("Please enter your monthly income: "))
+monthly_income = float(input("Please enter your monthly income: "))
 
 ##PROMPT THE USER TO ENTER THEIR EXPENSES
 
-expenses = float(input("Please enter your monthly expenses: "))
+monthly_expenses = float(input("Please enter your monthly expenses: "))
 
 
 ##CALCULATE THE MONTHLY SAVINGS
 
-monthly_savings = income - expenses
+monthly_savings = monthly_income - monthly_expenses
 
 Projected_Savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
