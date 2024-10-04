02f20e884a38cf9e28634a0370ed1ba2735a4640
Godwill Kiplagat <godwillkiprop@gmail.com>
2024-10-04 10:58:31 +0300

    unittest importedclear

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,15 +1,32 @@
-class SimpleCalculator:
+import unittest
+from simple_calculator import SimpleCalculator
 
-    def add(self, a, b):
-        return a + b
+class TestSimpleCalculator(unittest.TestCase):
 
-    def subtract(self, a, b):
-        return a - b
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
 
-    def multiply(self, a, b):
-        return a * b
-
-    def divide(self, a, b):
-        if b == 0:
-            return None
-        return a / b
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-2, 5), 3)
+        
+    def test_subtraction(self):
+        """Test the subtract method."""
+        self.assertEqual(self.calc.subtract(3, 2), 1)
+        self.assertEqual(self.calc.subtract(1, 1), 0)
+        self.assertEqual(self.calc.subtract(-5, 2), -3)    
+        
+    def test_multiplication(self):
+        """Test the multiply method."""
+        self.assertEqual(self.calc.multiply(3, 2), 6)
+        self.assertEqual(self.calc.multiply(1, -1), -1)
+        self.assertEqual(self.calc.multiply(5, 0), 0)
+        
+    def test_division(self):
+        """Test the divide method."""
+        self.assertEqual(self.calc.divide(4, 2), 2)
+        self.assertEqual(self.calc.divide(1, -1), -1)
+        self.assertEqual(self.calc.divide(5, 0), None)       
