ceed15528409f5bac85cd3db76dc79c904104f40
Godwill Kiplagat <godwillkiprop@gmail.com>
2024-10-11 17:23:57 +0300

    class_static_methods_demo.py

--- oop/mainClass_static.py
+++ oop/mainClass_static.py
@@ -2,7 +2,7 @@ from class_static_methods_demo import Calculator
 
 def main():
     # Using the static method
-    sum_result = Calculator.add(10, 5)
+    sum_result = Calculator.add(10, 5) 
     print(f"The sum is: {sum_result}")
 
     # Using the class method
