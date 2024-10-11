0f279b133c7ddac15959f98e1b7a19a2c4e7d1f9
Godwill Kiplagat <godwillkiprop@gmail.com>
2024-10-11 13:01:20 +0300

    created oop directory

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,7 +7,7 @@ def perform_operation(num1, num2, operation):
         return num1 - num2
     elif operation == 'multiply':
         return num1 * num2
-    elif operation == 'divide':
+    elif operatgition == 'divide':
         if num2 == 0:
             raise ValueError("Cannot divide by zero")
         return num1 / num2
